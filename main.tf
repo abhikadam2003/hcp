66811dc3a76e9bdbba28588f6529e4ed207b31ce
abhikadam2003 <kadamabhi7875@gmail.com>
2026-01-14 16:43:13 +0530

    Update AWS instance AMI and instance type

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-ami = "ami-0ced6a024bb18ff2e "
-instance_type = "t3.micro"
+ami = "ami-04808bdb94be6720e"
+instance_type = "t2.micro"
 tags = {
 Name = "hcp-server"
 }
