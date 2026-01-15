c95ff8a7defdeb0b8e7a466d5d5e2256d6fbe03a
abhikadam2003 <kadamabhi7875@gmail.com>
2026-01-15 15:39:57 +0530

    Update key_name for AWS instance resource

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count = 4
   ami           = "ami-02b8269d5e85954ef"
   instance_type = "c7i-flex.large"
-  key_name = " "
+  key_name = "keyy"
   vpc_security_group_ids = ["sg-0f4d276f8d5e946c5"]
   tags = {
     Name = var.instance_names[count.index]
