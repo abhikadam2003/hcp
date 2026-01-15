7be29818b1dcbfb146ba576997438c3163a364d6
abhikadam2003 <kadamabhi7875@gmail.com>
2026-01-15 15:38:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,19 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "one" {
+  count = 4
   ami           = "ami-02b8269d5e85954ef"
   instance_type = "c7i-flex.large"
-
+  key_name = " "
+  vpc_security_group_ids = ["sg-0f4d276f8d5e946c5"]
   tags = {
-    Name = "simple-ec2"
+    Name = var.instance_names[count.index]
   }
 }
+   
+
+  variable = "instance_names" {
+    default = ["jenkins","App-server-1","App-server-2","monituring"]
+  }
+
